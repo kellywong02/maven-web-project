98d915be3170de8790d39e5d9d6179aad96ca6c5
kellywong02 <kellylouvyu@gmail.com>
2021-09-21 06:10:05 -0700

    Hellloooooo

--- src/main/java/first/webapp/UserServlet.java
+++ src/main/java/first/webapp/UserServlet.java
@@ -15,8 +15,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
-
-
+//hihih
 
 /**
  * Servlet implementation class UserServlet
